ac63b853b0216652a5a843ac7a405fc381ff7596
Rogerio Colaco <colaco.rogerio@gmail.com>
2021-05-04 10:30:15 -0300

    - Commit inicial ead01 e projeto de aula

---
+++ ead01/src/app/web.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from './Produto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebService {
+
+  baseURL = "https://banco-dados-teste.glitch.me/api";
+
+  getProdutos() : Observable<Produto[]>{
+    return this.http.get<Produto[]>(this.baseURL+"/produtos");
+  }
+
+  constructor(private http : HttpClient) { }
+}
