0e4319cb35bfd480d9c1be4cd9905442025b4bb4
Rogerio Colaco <colaco.rogerio@gmail.com>
2021-05-06 22:27:45 -0300

    - complementos

--- segundo/src/app/app.module.ts
+++ segundo/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { ListPersonsComponent } from './list-persons/list-persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterFormComponent,
+    ListPersonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
