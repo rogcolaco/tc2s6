b0fe5513110c0935b12f4707c0ceba3ea3be0c2e
Rogerio Colaco <colaco.rogerio@gmail.com>
2021-05-27 22:48:19 -0300

    - Binding formulário

--- terceiro/src/app/form/form.component.ts
+++ terceiro/src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -11,12 +11,25 @@ export class FormComponent implements OnInit {
   formCadastro : FormGroup;
 
   cadastrar() {
-    
+    if(this.formCadastro.valid){
+      console.log("formulário válido");
+    } else {
+      console.log("formulário inválido");
+    }
+  }
+
+  initForm(){
+    this.formCadastro = new FormGroup({
+      nome: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      idade: new FormControl(0, [Validators.min(18)]),
+      sexo: new FormControl(null)
+    });
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
 }
